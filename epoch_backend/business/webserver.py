de1c669963e403f8f3407c0aa4e7bd166b70e0c5
aymanhki <agamyahk@myumanitoba.ca>
2024-02-10 11:31:22 -0600

    Attempt #104 to fix a deployment issue where the threads do not close after the tests pass

--- epoch_backend/business/webserver.py
+++ epoch_backend/business/webserver.py
@@ -49,7 +49,6 @@ class webserver:
 
                 except Exception as e:
                     print(f"Error handling request: {e}")
-                    conn.close()
 
         except KeyboardInterrupt:
             print("\n*** Server terminated by user. ***\n")
