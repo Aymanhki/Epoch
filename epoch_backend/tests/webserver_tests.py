8c18890b2a7effd4ab9d65811d06e8445f4bb792
cedricb12 <123005335+cedricb12@users.noreply.github.com>
2024-02-23 12:00:18 -0600

    Moved following integration tests
    added following integration tests to webserver tests file

--- epoch_backend/tests/webserver_tests.py
+++ epoch_backend/tests/webserver_tests.py
@@ -25,6 +25,7 @@ from epoch_backend.business.webserver import webserver
 from epoch_backend.business.utils import start_db_tables, get_google_credentials, terminate_processes_on_port
 
 class webserver_tests(unittest.TestCase):
+    unittest.TestLoader.sortTestMethodsUsing = None
     server_thread = None
     web_server = None
     username = str(uuid.uuid4())
@@ -109,6 +110,74 @@ class webserver_tests(unittest.TestCase):
         self.assertEqual(response_json['profile_pic_id'], 1)
         print(response_json)
         print("Got user info.")
+
+    def test_f1_get_all_user(self):
+        print("Getting all followable acccounts...")
+        response = requests.get('http://localhost:8080/api/follow/accountList/', cookies={'epoch_session_id': self.get_session_id()})
+        print("response:", response)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        print (response_json[0]["user_id"])
+        if len(response_json) > 0:
+            self.set_follow_id(int(response_json[0]["user_id"]))
+            print("Got all followable acccounts.")
+        else:
+            print("tests will not work with no other accounts in the db")
+            self.assertTrue(False)
+        
+
+    def test_f2_get_following_empty(self):
+        print("Getting empty following list...")
+        response = requests.get('http://localhost:8080/api/follow/followingList/', cookies={'epoch_session_id': self.get_session_id()})
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertEqual(len(response_json), 0)
+        print("Got empty following list.")
+
+    def test_f3_follow_user(self):
+        print("Following user...")
+        response = requests.post('http://localhost:8080/api/follow/follow/',
+                                 json={'userToFollow': self.get_follow_id()},
+                                  cookies={'epoch_session_id': self.get_session_id()})
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertTrue('user_id' in  response_json)
+        print("Followed user.")
+
+    def test_f4_get_following(self):
+        print("Getting following list...")
+        response = requests.get('http://localhost:8080/api/follow/followingList/', cookies={'epoch_session_id': self.get_session_id()})
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertNotEqual(len(response_json), 0)
+        self.assertEqual(response_json[0]['following_id'], self.get_follow_id())
+        self.assertFalse('user_id' in  response_json)
+        print("Got following list.")
+
+    def test_f5_get_followers(self):
+        # not implemented yet
+        pass
+
+    def test_f6_dupe_follow(self):
+        print("Attempting dupe follow...")
+        response = requests.post('http://localhost:8080/api/follow/follow/',
+                                 json={'userToFollow': self.user_to_follow_id},
+                                  cookies={'epoch_session_id': self.get_session_id()})
+        self.assertNotEqual(response.status_code, 200)
+        print("Attempted dupe follow.")
+
+    def test_f7_unfollow_user(self):
+        print("Unfollowing user...")
+        response = requests.post('http://localhost:8080/api/follow/unfollow/',
+                                 json={'userToUnfollow': self.get_follow_id()},
+                                  cookies={'epoch_session_id': self.get_session_id()})
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        response = requests.get('http://localhost:8080/api/follow/followingList/', cookies={'epoch_session_id': self.get_session_id()})
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertEqual(len(response_json), 0)
+        print("Unfollowed user.")
 
     def test_4_delete_user(self):
         print("Deleting user...")
