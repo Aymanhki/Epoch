fa9ced226071d99dd15cf69a5d1f6f19f2e2b7ed
cedricb12 <123005335+cedricb12@users.noreply.github.com>
2024-02-24 15:50:53 -0600

    Added Search Function to Search page
    - added searching to userlist page
    - added follow button back to userlist page
    - changed how userlist updates so page no longer refreshes when an account is followed

--- epoch_frontend/src/pages/userlist.js
+++ epoch_frontend/src/pages/userlist.js
@@ -1,20 +1,54 @@
 import { followAccount, unfollowAccount } from "../services/following";
 import { fillUserList } from "../services/new_following";
 import React, {useState, useEffect, useContext} from 'react';
+import {redirect, useNavigate} from "react-router-dom";
 import {Spinner} from '../modules/Spinner'
-import {useNavigate} from "react-router-dom";
 import NavBar from "../modules/NavBar";
 import {UserContext} from "../services/UserContext";
-import {getUserInfo} from "../services/user";
+import {getUserInfo,getUsernameInfo} from "../services/user";
+import { TextField } from "@mui/material";
 
 function Userlist() {
     const [isLoading, setIsLoading] = useState(false);
     const [userList, setUserList] = useState({});
+    const [filteredList, setFilteredList] = useState(null);
+    const [changedStatus, changeStatus] = useState(false);
     const [showNewPostPopup, setShowNewPostPopup] = useState(false);
     const { user } = useContext(UserContext);
     const { updateUser } = useContext(UserContext);
     const navigate = useNavigate();
+    const [searchInput, setSearchInput] = useState("");
 
+    let handleChange = (event) => {
+        setSearchInput(event.target.value.toLowerCase());
+    };
+    
+    function getFollowingPrompt(isFollowing) {
+        if (isFollowing) {
+            return "Unfollow";
+        }
+        return "Follow";
+    }
+
+    function handleFollow(target, isFollowing) {
+        console.log(target, isFollowing, "following");
+        if (isFollowing){
+            unfollowAccount(target);
+        }
+        else {
+            followAccount(target);
+        }
+        for (var i in userList) {
+            if (userList[i].user_id === target) {
+                userList[i].isFollowing = !isFollowing;
+            }
+        }
+        userList.sort(function(a,b){
+            return b.isFollowing - a.isFollowing;
+        });
+        setUserList(userList);
+        changeStatus(!changedStatus);
+    }
     
     useEffect(() => {
         setIsLoading(true);
@@ -33,10 +67,14 @@ function Userlist() {
     
     useEffect(() => {
         setIsLoading(true);
+        if (!user) {
+            updateUser(null);
+        }
         fillUserList()
         .then(data => {
             setUserList(data);
             setIsLoading(false);
+            changeStatus(false);
         })
         .catch(error => {
             setIsLoading(false);
@@ -45,36 +83,91 @@ function Userlist() {
         });
     },[setUserList, navigate]);
 
+    useEffect(() => {
+        changeStatus(false);
+    },[changeStatus]);
 
+    useEffect(() => {
+        if(searchInput === '') {
+            setFilteredList(null);
+        }
+        else {
+            var tempFiltered = [];
+    
+            for(var i in userList) {
+                if( userList[i].username.toLowerCase().includes(searchInput) ) {
+                    tempFiltered.push(userList[i]);
+                }
+            }
+            setFilteredList(tempFiltered);
+        }
+        console.log(filteredList);
+        changeStatus(!changedStatus);
+    },[searchInput]);
+
+    useEffect(() => {
+        if(!user){
+            navigate('/epoch/profile/');
+        }
+    },[redirect]);
 
     return (
         <>
-        <NavBar profilePic={user.profile_pic_data} profilePicType={user.profile_pic_type} showNewPostPopup={showNewPostPopup} setShowNewPostPopup={setShowNewPostPopup} />
-            {isLoading ? <Spinner/>: (
-                <div>
-                    <h1>List of all Epoch Users</h1>
-                    <ul>
-                        {userList && userList.map && userList.map(account =>
-                            <li key = {account.user_id}>
-                                <p>
-                                    <b>{account.username}: </b>
-                                    {account.isFollowing ? <b>(following)</b>:<b> </b>}
-                                     
-                                </p>
+            {!user ?  (navigate('/epoch/profile/')) : (
+                isLoading ? <Spinner/> : (
+                    <div className="UserListContainer">
+                        <NavBar profilePic={user.profile_pic_data} profilePicType={user.profile_pic_type} showNewPostPopup={showNewPostPopup} setShowNewPostPopup={setShowNewPostPopup} />
+                        <h1>List of all Epoch Users</h1>
 
-                                <div>
-                                    
-                                    <button type="button" onClick = {() => {
-                                        navigate('/epoch/'+ account.username)
-                                    }}>view profile</button>
-                                </div>
-                            </li>
+                        <TextField className="search-bar"
+                            variant="outlined"
+                            label = "Search users."
+                            onChange={handleChange}
+                        />
+                        <ul className="user-list">
+                            {filteredList && searchInput ? (
+                                filteredList && filteredList.map && filteredList.map(account =>
+                                    <li key = {account.user_id} className="user-list-items">
+                                        <p>
+                                            <b className="username">{account.username}: </b>
+                                            {account.isFollowing ? <b className="following-status">(following)</b>:<b> </b>}
+                                        </p>
+        
+                                        <div>
+                                            <button type="button" className="search-follow button" onClick = {() => {
+                                                handleFollow( account.user_id, account.isFollowing)
+                                            }}>{getFollowingPrompt(account.isFollowing)}</button>
+                                            <button type="button" className="profile-button" onClick = {() => {
+                                                navigate('/epoch/'+ account.username)
+                                            }}>view profile</button>
+                                        </div>
+                                    </li>
+                                    )
+                            ):(
+                                userList && userList.map && userList.map(account =>
+                                    <li key = {account.user_id} className="user-list-items">
+                                        <p>
+                                            <b className="username">{account.username}: </b>
+                                            {account.isFollowing ? <b className="following-status">(following)</b>:<b> </b>}
+                                        </p>
+        
+                                        <div>
+                                            <button type="button" className="search-follow button" onClick = {() => {
+                                                handleFollow( account.user_id, account.isFollowing)
+                                            }}>{getFollowingPrompt(account.isFollowing)}</button>
+                                            <button type="button" className="profile-button" onClick = {() => {
+                                                navigate('/epoch/'+ account.username)
+                                            }}>view profile</button>
+                                        </div>
+                                    </li>
+                                    )
                             )}
-                    </ul> 
-                </div>
-                
+                            
+                        </ul>
+                    </div>
+                    
+                )
             )}
-
         </>
     );
 }
