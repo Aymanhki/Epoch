0b992c53541d766ad3570a093f03a6e5abf9eb98
aymanhki <agamyahk@myumanitoba.ca>
2024-03-28 14:30:16 -0500

    Improved feed

--- epoch_frontend/src/modules/Feed.js
+++ epoch_frontend/src/modules/Feed.js
@@ -3,7 +3,6 @@ import '../styles/Feed.css';
 import {Spinner} from "./Spinner";
 import Post from "./Post";
 import {getAllUserPosts, getFollowedUsersPost, getAllHashtagPosts} from '../services/post.js'
-import { ViewportList } from 'react-viewport-list';
 import {useRef} from "react";
 
 
@@ -25,220 +24,188 @@ export default function Feed({
 
     const [isLoading, setIsLoading] = useState(true);
     const [feedPosts, setFeedPosts] = useState([]);
-    const [offset, setOffset] = useState(0);
-    const [limit, setLimit] = useState(30);
     const [noMorePosts, setNoMorePosts] = useState(false);
     const [firstRender, setFirstRender] = useState(true);
     const [ref, setRef] = useState(useRef(null));
-    const [loadingTop, setLoadingTop] = useState(false);
+    const [loadingTop, setLoadingTop] = useState(true);
     const [topOffset, setTopOffset] = useState(0);
     const [allowScrollToTop, setAllowScrollToTop] = useState(false);
     const [noMoreTopPosts, setNoMoreTopPosts] = useState(false);
-    const [removeTop, setRemoveTop] = useState(false);
-    const maxPosts = 50;
-    const toRemove = 10;
+    const [offset, setOffset] = useState(0);
+    const [limit, setLimit] = useState(20); // must be bigger tha maxPosts
+    const maxPosts = 25;
+    const toRemove = 15; // Must be smaller than maxPosts
 
-
-    const refreshFeedPosts =  (reset, fromTop) => {
+    const refreshFeedPosts =  (reset, fromTop) =>
+    {
         let finalOffset = reset ? 0 : (fromTop ? topOffset : offset);
         let finalFeedPosts = reset ? [] : feedPosts;
 
         if (reset) {
             setFeedPosts([]);
-            setOffset(0);
             setNoMorePosts(false);
             setNoMoreTopPosts(false);
             setAllowScrollToTop(false);
-            setRemoveTop(false);
             setTopOffset(0);
         }
 
-        if (!posts) {
-            if (isInHashtags && hashtag) {
-                getAllHashtagPosts(hashtag, finalOffset, limit).then((data) => {
+        if (!posts)
+        {
+            if (isInHashtags && hashtag)
+            {
+                getAllHashtagPosts(hashtag, finalOffset, limit)
+                .then((data) =>
+                {
+                    if (!fromTop)
+                    {
+                        onNewBottomPosts(data, finalFeedPosts, finalOffset);
+                    }
+                    else
+                    {
+                        onNewTopPosts(data, finalFeedPosts, finalOffset);
+                    }
+                })
+                .catch((error) =>
+                {
+                    console.log(error);
 
-                    if (!fromTop) {
-                        setFeedPosts(finalFeedPosts.concat(data));
-
-                        if (data.length < limit) {
-                            setNoMorePosts(true);
-                        }
-
-                        setRemoveTop(true);
-                        setOffset(finalOffset + limit)
+                    if(!fromTop)
+                    {
                         setIsLoading(false);
-                    } else {
-                        setFeedPosts(data.concat(finalFeedPosts));
-
-                        if (topOffset <= 0) {
-                            setNoMoreTopPosts(true);
-                            setAllowScrollToTop(false);
-                        }
-
-                        setRemoveTop(false);
-                        setTopOffset(finalOffset - limit)
+                    }
+                    else
+                    {
                         setLoadingTop(false);
                     }
-
-                }).catch((error) => {
-                    console.error(error);
-
-                    if(!fromTop) {
-                        setIsLoading(false);
-                    } else {
-                        setLoadingTop(false);
-                    }
-
                 });
-            } else if (currentUser) {
-                if (isInProfile && feedUserId && currentUser.id === feedUserId) {
-                    getAllUserPosts(currentUser.id, finalOffset, limit).then((data) => {
-
-                        if (!fromTop) {
-                            setFeedPosts(finalFeedPosts.concat(data));
-                            setOffset(finalOffset + limit)
-
-                            if (data.length < limit) {
-                                setNoMorePosts(true);
-                            }
-
-                            setRemoveTop(true);
-                            setIsLoading(false);
-                        } else {
-                            setFeedPosts(data.concat(finalFeedPosts));
-                            setTopOffset(finalOffset - limit);
-
-                            if (data.length < limit) {
-                                setNoMoreTopPosts(true);
-                                setAllowScrollToTop(false);
-                            }
-
-                            setRemoveTop(false);
-                            setLoadingTop(false);
+            }
+            else if (currentUser)
+            {
+                if (isInProfile && feedUserId && currentUser.id === feedUserId)
+                {
+                    getAllUserPosts(currentUser.id, finalOffset, limit)
+                    .then((data) =>
+                    {
+                        if (!fromTop)
+                        {
+                            onNewBottomPosts(data, finalFeedPosts, finalOffset);
                         }
-
-                    }).catch((error) => {
-                        console.error(error);
-
-                        if(!fromTop) {
-                            setIsLoading(false);
-                        } else {
-                            setLoadingTop(false);
+                        else
+                        {
+                            onNewTopPosts(data, finalFeedPosts, finalOffset);
                         }
-                    });
-                } else if (feedUserId && currentUser.id !== feedUserId) {
-                    getAllUserPosts(feedUserId, finalOffset, limit).then((data) => {
+                    })
+                    .catch((error) =>
+                    {
+                        console.log(error);
 
-                        if (!fromTop) {
-                            setFeedPosts(finalFeedPosts.concat(data));
-                            setOffset(finalOffset + limit)
-
-                            if (data.length < limit) {
-                                setNoMorePosts(true);
-                            }
-
-                            setRemoveTop(true);
+                        if(!fromTop)
+                        {
                             setIsLoading(false);
-                        } else {
-                            setFeedPosts(data.concat(finalFeedPosts));
-                            setTopOffset(finalOffset - limit);
-
-                            if (data.length < limit) {
-                                setNoMoreTopPosts(true);
-                                setAllowScrollToTop(false);
-                            }
-
-                            setRemoveTop(false);
-                            setLoadingTop(false);
                         }
-                    }).catch((error) => {
-                        console.error(error);
-
-                        if(!fromTop) {
-                            setIsLoading(false);
-                        } else {
-                            setLoadingTop(false);
-                        }
-                    });
-                } else if (!isInProfile && feedUsername && currentUser.username === feedUsername) {
-                    getFollowedUsersPost(currentUser.id, finalOffset, limit).then((data) => {
-
-
-                        if (!fromTop) {
-                            setFeedPosts(finalFeedPosts.concat(data));
-                            setOffset(finalOffset + limit)
-
-                            if (data.length < limit) {
-                                setNoMorePosts(true);
-                            }
-
-                            setRemoveTop(true);
-                            setIsLoading(false);
-                        } else {
-                            setFeedPosts(data.concat(finalFeedPosts));
-                            setTopOffset(finalOffset - limit);
-
-                            if (data.length < limit) {
-                                setNoMoreTopPosts(true);
-                                setAllowScrollToTop(false);
-                            }
-
-                            setRemoveTop(false);
-                            setLoadingTop(false);
-                        }
-
-                    }).catch((error) => {
-                        console.error(error);
-
-                        if(!fromTop) {
-                            setIsLoading(false);
-                        } else {
+                        else
+                        {
                             setLoadingTop(false);
                         }
                     });
                 }
-            } else {
-                if (feedUserId) {
-                    getAllUserPosts(feedUserId, finalOffset, limit).then((data) => {
+                else if (feedUserId && currentUser.id !== feedUserId)
+                {
+                    getAllUserPosts(feedUserId, finalOffset, limit)
+                    .then((data) =>
+                    {
+                        if (!fromTop)
+                        {
+                            onNewBottomPosts(data, finalFeedPosts, finalOffset);
+                        }
+                        else
+                        {
+                            onNewTopPosts(data, finalFeedPosts, finalOffset);
+                        }
+                    })
+                    .catch((error) =>
+                    {
+                        console.log(error);
 
-
-                        if (!fromTop) {
-                            setFeedPosts(feedPosts.concat(data));
-                            setOffset(finalOffset + limit)
-
-                            if (data.length < limit) {
-                                setNoMorePosts(true);
-                            }
-
-                            setRemoveTop(true);
+                        if(!fromTop)
+                        {
                             setIsLoading(false);
-                        } else {
-                            setFeedPosts(data.concat(feedPosts));
-                            setTopOffset(finalOffset - limit);
-
-                            if (data.length < limit) {
-                                setNoMoreTopPosts(true);
-                                setAllowScrollToTop(false);
-                            }
-
-                            setRemoveTop(false);
+                        }
+                        else
+                        {
                             setLoadingTop(false);
                         }
-                    }).catch((error) => {
-                        console.error(error);
+                    });
+                }
+                else if (!isInProfile && feedUsername && currentUser.username === feedUsername)
+                {
+                    getFollowedUsersPost(currentUser.id, finalOffset, limit)
+                    .then((data) =>
+                    {
+                        if (!fromTop)
+                        {
+                            onNewBottomPosts(data, finalFeedPosts, finalOffset);
+                        }
+                        else
+                        {
+                            onNewTopPosts(data, finalFeedPosts, finalOffset);
+                        }
+                    })
+                    .catch((error) =>
+                    {
+                        console.log(error);
 
-                        if(!fromTop) {
+                        if(!fromTop)
+                        {
                             setIsLoading(false);
-                        } else {
+                        }
+                        else
+                        {
                             setLoadingTop(false);
                         }
                     });
                 }
             }
-        } else {
-            if (!fromTop) {
+            else
+            {
+                if (feedUserId)
+                {
+                    getAllUserPosts(feedUserId, finalOffset, limit)
+                    .then((data) =>
+                    {
+                        if (!fromTop)
+                        {
+                            onNewBottomPosts(data, finalFeedPosts, finalOffset);
+                        }
+                        else
+                        {
+                            onNewTopPosts(data, finalFeedPosts, finalOffset);
+                        }
+                    })
+                    .catch((error) =>
+                    {
+                        console.log(error);
+
+                        if(!fromTop)
+                        {
+                            setIsLoading(false);
+                        }
+                        else
+                        {
+                            setLoadingTop(false);
+                        }
+                    });
+                }
+            }
+        }
+        else
+        {
+            if (!fromTop)
+            {
                 setIsLoading(false);
-            } else {
+            }
+            else
+            {
                 setLoadingTop(false);
             }
 
@@ -246,6 +213,73 @@ export default function Feed({
         }
 
         setRefreshFeed(false);
+    }
+
+    const onNewBottomPosts = (data, finalFeedPosts, finalOffset) =>
+    {
+        let finalToSetFeedPosts = finalFeedPosts;
+
+        if (data.length + finalFeedPosts.length > maxPosts)
+        {
+            finalToSetFeedPosts = finalToSetFeedPosts.slice(toRemove);
+            finalToSetFeedPosts = finalToSetFeedPosts.concat(data);
+            let newTopOffset = (offset - limit - limit + (maxPosts - limit)) < 0 ? 0 : (offset - limit - limit + (maxPosts - limit));
+            setTopOffset(newTopOffset);
+            setAllowScrollToTop(true);
+        }
+        else
+        {
+            finalToSetFeedPosts = finalToSetFeedPosts.concat(data);
+        }
+
+        setFeedPosts(finalToSetFeedPosts);
+
+        if (data.length < limit)
+        {
+            setNoMorePosts(true);
+        }
+
+        setOffset(finalOffset + limit)
+        setIsLoading(false);
+        setLoadingTop(false);
+    }
+
+    const onNewTopPosts = (data, finalFeedPosts, finalOffset) =>
+    {
+        let finalToSetFeedPosts = finalFeedPosts;
+
+        if (data.length + finalFeedPosts.length > maxPosts)
+        {
+            finalToSetFeedPosts = finalToSetFeedPosts.slice(0, (limit));
+            finalToSetFeedPosts = data.concat(finalToSetFeedPosts);
+
+            if (noMoreTopPosts)
+            {
+                setOffset(topOffset - limit - limit);
+            }
+            else
+            {
+                setOffset(topOffset - limit - limit);
+            }
+
+            setNoMorePosts(false);
+        }
+        else
+        {
+            finalToSetFeedPosts = data.concat(finalToSetFeedPosts);
+        }
+
+        setFeedPosts(finalToSetFeedPosts);
+
+        if (topOffset <= 0 || data.length < limit)
+        {
+            setNoMoreTopPosts(true);
+            setAllowScrollToTop(false);
+        }
+
+        setTopOffset((finalOffset - limit) < 0 ? 0 : (finalOffset - limit));
+        setLoadingTop(false);
+        setIsLoading(false);
     }
 
     useEffect(() => {
@@ -263,55 +297,48 @@ export default function Feed({
     }, [refreshFeed, refreshFeedPosts]);
 
     window.onscroll = () => {
-        if (window.innerHeight + document.documentElement.scrollTop >= (document.documentElement.offsetHeight/2)) {
+        if ((window.innerHeight + document.documentElement.scrollTop) >= document.documentElement.offsetHeight) {
             if (!noMorePosts && !isLoading) {
                 setIsLoading(true);
                 refreshFeedPosts(false, false);
             }
         }
-        else if (window.innerHeight + document.documentElement.scrollTop <= (document.documentElement.offsetHeight/2) ) {
-            if (!noMoreTopPosts && allowScrollToTop && !loadingTop) {
-                setLoadingTop(true);
-                refreshFeedPosts(false, true);
-            }
-        }
     }
-
-    useEffect(() => {
-        if (feedPosts.length > maxPosts && removeTop) {
-            setFeedPosts(feedPosts.slice(toRemove));
-            setTopOffset(maxPosts - toRemove);
-            setAllowScrollToTop(true);
-        }
-        else if (feedPosts.length > maxPosts) {
-            setFeedPosts(feedPosts.slice(0, maxPosts));
-            setNoMorePosts(false);
-
-        }
-    }, [feedPosts]);
-
-
 
     return (
         <>
-            <div className={'feed-wrapper'}>
+            {(isLoading && loadingTop) ? <Spinner className={'feed-loading'}/> : (
+                <div className={'feed-wrapper'}>
                 {loadingTop && <Spinner className={'feed-loading'}/>}
+
                 <div className={'posts-wrapper'}>
+                    {(!loadingTop && allowScrollToTop && !noMoreTopPosts) && (
+                        <div className={'load-more-top-buttons-wrapper'}>
+                            <button className={'reset-feed-button'} onClick={() => {
+                                setLoadingTop(true);
+                                setIsLoading(true);
+                                refreshFeedPosts(true, false);
+                            }}>Refresh feed (top)</button>
+
+                            {/*<button className={'load-previous-posts-button'} onClick={() => {*/}
+                            {/*    setLoadingTop(true);*/}
+                            {/*    refreshFeedPosts(false, true);*/}
+                            {/*}}>Load previous posts*/}
+                            {/*</button>*/}
+                        </div>
+                    )}
+
                     {(feedPosts.length === 0 && !isLoading) &&
-                        <div className={'no-posts'}>No posts to show, follow some people or make a new
-                            post</div>}
+                        <div className={'no-posts'}>No posts to show, follow some people or make a new post</div>}
 
-                    {feedPosts.length > 0 &&
-                    <div className="scroll-container" ref={ref}>
-                        <ViewportList viewportRef={ref}  items={feedPosts} >
-                            {(item) => (
-                                <Post key={item.post_id} post={item} postViewer={currentUser} refreshFeed={refreshFeed}
-                                            setRefreshFeed={setRefreshFeed} isInFavorites={isInFavorites}/>
-                            )}
-                        </ViewportList>
-                        {noMorePosts && <h3 className={'no-more-posts'}>No more posts to show</h3>}
-                    </div>}
+                    {feedPosts.length > 0 && (
+                        feedPosts.map((item) => (
+                            <Post key={item.post_id} post={item} postViewer={currentUser} refreshFeed={refreshFeed}
+                                  setRefreshFeed={setRefreshFeed} isInFavorites={isInFavorites}/>
+                        ))
+                    )}
 
+                    {noMorePosts && <h3 className={'no-more-posts'}>No more posts to show</h3>}
                 </div>
 
                 {(currentUser && ((!isInProfile && feedUsername && currentUser.username === feedUsername) || (isInProfile && feedUserId && currentUser.id === feedUserId)) && !viewingOnly) && (
@@ -320,6 +347,7 @@ export default function Feed({
 
                 {isLoading && (<Spinner className={'feed-loading'}/>)}
             </div>
+            )}
         </>
     )
 }
